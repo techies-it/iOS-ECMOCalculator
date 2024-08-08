974975b15ffa9327133a8055a3d7ee386ed11247
ms-tii <mohit@techies.it>
2024-08-08 14:26:28 +0530

    Initial Commit

---
+++ Ecmo Clinical Calculator/Ecmo_Clinical_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecmo_Clinical_CalculatorApp.swift
+//  Ecmo Clinical Calculator
+//
+//  Created by Mohit Sharma on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Ecmo_Clinical_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
